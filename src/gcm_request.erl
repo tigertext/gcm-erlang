3e55bb117e9961d7a321936361a07bd82254118e
zpeng01 <zpeng@tigerconnect.com>
2024-04-08 15:44:01 +0800

    update

--- src/gcm_request.erl
+++ src/gcm_request.erl
@@ -49,10 +49,10 @@ send_from_project({ProjectId, Auth, RegIds, Message}, {_Key, _ErrorFun}) ->
                 end,
     Android = [{<<"android">>, TtlList ++ PriorityList}],
 
-    lager:info("[WIP] FCM Project sending push: Url=~p \n Data=~p \n Android=~p \n RegIds=~p", [Url, Data, Android, RegIds]),
+    lager:info("[WIP] FCM Project sending push: Url=~p \n Data=~p \n Android=~p \n RegIds=~p", [Url, NewData, Android, RegIds]),
     [
         begin
-            Body =[{<<"message">>, [{<<"tokens">>, RegId}, {<<"data">>, Data}] ++ Android}],
+            Body =[{<<"message">>, [{<<"tokens">>, RegId}, {<<"data">>, NewData}] ++ Android}],
             Headers = [{"Authorization", string:concat("Bearer ", binary_to_list(Auth))}],
 
             case json_post_request(Url, Headers, Body) of

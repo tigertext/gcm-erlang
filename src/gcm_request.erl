9d291975fa92a5d202e8922aa9db2eddbccb60eb
zpeng01 <zpeng@tigerconnect.com>
2024-06-18 17:59:34 +0800

    update

--- src/gcm_request.erl
+++ src/gcm_request.erl
@@ -75,7 +75,7 @@ send_from_project({ProjectId, Auth, RegIds, Message}, {_Key, ErrorFun}) ->
                     {http_error, Code} = OtherError when Code == 403 orelse Code == 404 ->
                         lager:info([{sender_id, SenderId}, {account_token, ReceiverId}, {resource_token, ResourceId}, {status_code, Code}], 
                                     "FCM Project push sent failed: ~p~n", [OtherError]),
-                        ErrorFun(<<"InvalidRegistration">>, RegId, Message),
+                        spawn(fun() -> ErrorFun(<<"InvalidRegistration">>, RegId, Message) end),
                         ok;
                     {http_error, Code} = OtherError when Code >= 400 andalso Code =< 499 ->
                         lager:info([{sender_id, SenderId}, {account_token, ReceiverId}, {resource_token, ResourceId}, {status_code, Code}], 
@@ -84,7 +84,7 @@ send_from_project({ProjectId, Auth, RegIds, Message}, {_Key, ErrorFun}) ->
                     {http_error, Code} = OtherError when Code >= 500 andalso Code =< 599 ->
                         lager:info([{sender_id, SenderId}, {account_token, ReceiverId}, {resource_token, ResourceId}, {status_code, Code}], 
                                    "FCM Project push sent failed: ~p~n", [OtherError]),
-                        ErrorFun(<<"Unavailable">>, RegId, Message),
+                        spawn(fun() -> ErrorFun(<<"Unavailable">>, RegId, Message) end),
                         error;
                     OtherError ->
                         lager:info([{sender_id, SenderId}, {account_token, ReceiverId}, {resource_token, ResourceId}], 
